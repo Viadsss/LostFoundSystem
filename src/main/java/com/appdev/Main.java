efe53cfb059c772bfbc9cdb5afe4166924c432a5
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-12-13 00:44:48 +0800

    Add default font, flatlaf theme, and insepctors

--- src/main/java/com/appdev/Main.java
+++ src/main/java/com/appdev/Main.java
@@ -1,10 +1,26 @@
 package com.appdev;
 
 import com.appdev.presentation.MainFrame;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.extras.FlatInspector;
+import com.formdev.flatlaf.extras.FlatUIDefaultsInspector;
+import com.formdev.flatlaf.fonts.inter.FlatInterFont;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import java.awt.Font;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Main {
   public static void main(String[] args) {
+    FlatInspector.install("ctrl shift alt X");
+    FlatUIDefaultsInspector.install("ctrl shift alt Y");
+
+    FlatLaf.registerCustomDefaultsSource("themes");
+    FlatMacLightLaf.setup();
+
+    FlatInterFont.install();
+    UIManager.put("defaultFont", new Font(FlatInterFont.FAMILY, Font.PLAIN, 13));
+
     SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
   }
 }
