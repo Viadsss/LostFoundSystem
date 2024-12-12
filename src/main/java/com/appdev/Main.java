4f2d836c46f30a2a399ea26b3f802931de308c41
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-12-13 00:40:52 +0800

    Add Page Manager

--- src/main/java/com/appdev/Main.java
+++ src/main/java/com/appdev/Main.java
@@ -1,5 +1,10 @@
 package com.appdev;
 
+import com.appdev.presentation.MainFrame;
+import javax.swing.SwingUtilities;
+
 public class Main {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
+  }
 }
