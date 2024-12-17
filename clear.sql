3f3aeb8d666c03a89e8970a5bd54258a1880abd1
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-12-17 18:45:18 +0800

    Update sql helper queries

--- clear.sql
+++ clear.sql
@@ -1,10 +1,18 @@
--- Clear the lost_items table
-DELETE FROM lost_items;
+USE lost_and_found;
 
--- Clear the found_items table
-DELETE FROM found_items;
+-- Disable foreign key checks
+SET foreign_key_checks = 0;
 
 -- Clear the match_items table
-DELETE FROM match_items;
+TRUNCATE TABLE match_items;
+
+-- Clear the lost_items table
+TRUNCATE TABLE lost_items;
+
+-- Clear the found_items table
+TRUNCATE TABLE found_items;
+
+-- Enable foreign key checks
+SET foreign_key_checks = 1;
 
 SELECT 'All table data cleared successfully.' AS STATUS;
