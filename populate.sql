7ffac5626639fbc018512273e0d4ff8efcb1f1fa
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-12-19 11:13:51 +0800

    update query to simulate no image in lost item

--- populate.sql
+++ populate.sql
@@ -4,7 +4,7 @@ USE lost_and_found;
 INSERT INTO lost_items (item_type, item_subtype, item_description, location_details, date_time_lost, item_photo_path, reporter_name, reporter_email, reporter_phone, status)
 VALUES
 ('Electronics', 'Smartphone', 'Black iPhone 13 with a cracked screen', 'Near the park bench in Central Park', '2024-11-10 14:00:00', '1.jpg', 'John Doe', 'john.doe@example.com', '12345678901', 'PENDING'),
-('Accessories', 'Wallet', 'Brown leather wallet with credit cards and IDs', 'Metro Station - Platform 2', '2024-12-01 08:30:00', '2.jpg', 'Jane Smith', 'jane.smith@example.com', '09876543211', 'PENDING'),
+('Accessories', 'Wallet', 'Brown leather wallet with credit cards and IDs', 'Metro Station - Platform 2', '2024-12-01 08:30:00', NULL, 'Jane Smith', 'jane.smith@example.com', '09876543211', 'PENDING'),
 ('Clothing', 'Jacket', 'Black North Face jacket, size L', 'Coffee shop near Elm Street', '2024-12-05 18:00:00', '3.jpg', 'Alice Johnson', 'alice.johnson@example.com', NULL, 'PENDING');
 
 -- Populate the found_items table
