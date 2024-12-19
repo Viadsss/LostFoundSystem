72c37f1e1263c04d706943f930fe6c6e9889784e
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-12-19 14:46:36 +0800

    add a method to get the match item by its id

--- src/main/java/com/appdev/data/dao/MatchItemDAO.java
+++ src/main/java/com/appdev/data/dao/MatchItemDAO.java
@@ -43,4 +43,31 @@ public class MatchItemDAO {
 
     return matchItems;
   }
+
+  public MatchItem getMatchItemById(int id) {
+    String query = "SELECT * FROM match_items WHERE match_id = ?";
+    MatchItem item = null;
+
+    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+      pstmt.setInt(1, id);
+
+      ResultSet rs = pstmt.executeQuery();
+      if (rs.next()) {
+        item =
+            new MatchItem(
+                rs.getInt("match_id"),
+                rs.getInt("lost_item_id"),
+                rs.getInt("found_item_id"),
+                rs.getString("id_photo_path"),
+                rs.getString("profile_path"),
+                MatchItem.Status.valueOf(rs.getString("status")),
+                rs.getTimestamp("createdAt").toLocalDateTime());
+      }
+
+    } catch (SQLException e) {
+      Logger.error(e, "Error retrieving match item with ID {} from the database.", id);
+    }
+
+    return item;
+  }
 }
