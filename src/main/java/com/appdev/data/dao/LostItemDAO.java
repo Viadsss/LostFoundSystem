97b36800037edafc6a600eeaf89836b5e4e27e51
lemevei <wendeldedios2122@gmail.com>
2024-12-26 19:27:16 +0800

    added addLostItemDAO

--- src/main/java/com/appdev/data/dao/LostItemDAO.java
+++ src/main/java/com/appdev/data/dao/LostItemDAO.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.tinylog.Logger;
@@ -154,4 +155,30 @@ public class LostItemDAO {
           e, "Error updating status of found item with ID {} in the database.", lostItemId);
     }
   }
+  
+  
+  public void addLostItem(String itemType, String itemSubtype, String itemDescription, String locationDetails, LocalDateTime dateTimeReported, String itemPhotoPath, String reporterName, String reporterEmail, String reporterPhone) {
+    String query =
+        "INSERT INTO lost_items (item_type, item_subtype, item_description, location_details, "
+            + "date_time_lost, item_photo_path, reporter_name, reporter_email, reporter_phone) "
+            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+        pstmt.setString(1, itemType);
+        pstmt.setString(2, itemSubtype);
+        pstmt.setString(3, itemDescription);
+        pstmt.setString(4, locationDetails);
+        pstmt.setTimestamp(5, Timestamp.valueOf(dateTimeReported)); // LocalDateTime to Timestamp
+        pstmt.setString(6, itemPhotoPath);
+        pstmt.setString(7, reporterName);
+        pstmt.setString(8, reporterEmail);
+        pstmt.setString(9, reporterPhone);
+        pstmt.executeUpdate();
+
+    } catch (SQLException e) {
+        Logger.error(e, "Error adding a new found item with status REPORTED to the database.");
+    }
+}
+
+  
 }
